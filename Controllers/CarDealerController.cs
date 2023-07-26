c95a558139f989e6d86918978ea3e9fd887558b5
SunshineMocha <127298037+SunshineMocha@users.noreply.github.com>
2023-07-26 14:39:55 +0200

    Added features
    - Added file creation for storage
    - fixed PUT Car
    - Added PUT Dealer

--- Controllers/CarDealerController.cs
+++ Controllers/CarDealerController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Net;
-//using System.Web.Http.Controllers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace API_Auto_v3._0.Controllers
 {
@@ -13,11 +15,75 @@ namespace API_Auto_v3._0.Controllers
     {
         private static List<CarDealer> carDealers = new List<CarDealer>();
 
+        private static string filePath = "data.txt";
+
+        private static void ReadRecords()
+        {
+            if (System.IO.File.Exists(filePath))
+            {
+                string[] lines = System.IO.File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    string[] recordData = line.Split(";");
+                    int dealerId = Convert.ToInt32(recordData[0]);
+                    string dealerName = recordData[1];
+                    List<Car> carList = new List<Car>();
+
+                    for (int i = 2; i < recordData.Length; i += 4)
+                    {
+                        string carPlate = recordData[i];
+                        string model = recordData[i + 1];
+                        int maxSpeed = Convert.ToInt32(recordData[i + 2]);
+                        double displacement = Convert.ToDouble(recordData[i + 3]);
+
+                        Car car = new Car
+                        {
+                            CarPlate = carPlate,
+                            Model = model,
+                            MaxSpeed = maxSpeed,
+                            Displacement = displacement
+                        };
+                        carList.Add(car);
+                    }
+
+                    CarDealer carDealer = new CarDealer
+                    {
+                        DealerID = dealerId,
+                        DealerName = dealerName,
+                        CarList = carList
+                    };
+
+                    carDealers.Add(carDealer);
+                }
+            }
+        }
+
+        private static void WriteRecords()
+        {
+            List<string> lines = new List<string>();
+            foreach (CarDealer carDealer in carDealers)
+            {
+                string recordData = $"{carDealer.DealerID};{carDealer.DealerName}";
+                foreach (Car car in carDealer.CarList)
+                {
+                    recordData += $";{car.CarPlate};{car.Model};{car.MaxSpeed};{car.Displacement}";
+                }
+
+                lines.Add(recordData);
+            }
+            System.IO.File.WriteAllLines(filePath, lines);
+        }
+
         // Getting ALL the car dealers
         // GET api/CarDealer
+
         [HttpGet]
         public ActionResult<IEnumerable<CarDealer>> Get()
         {
+            carDealers = new List<CarDealer>();
+            //CODICE PER READ FILE
+            ReadRecords();
+            //FINE CODICE READ FILE
             return carDealers;
         }
 
@@ -34,12 +100,15 @@ namespace API_Auto_v3._0.Controllers
             return carDealer;
         }
 
-        // Creating a car dealer with one (or more) cars
+        // Creating a car dealer
         // POST api/CarDealer
         [HttpPost]
         public ActionResult<CarDealer> Post(CarDealer carDealer)
         {
             carDealers.Add(carDealer);
+            //CODICE PER WRITE FILE
+            WriteRecords();
+            //FINE CODICE WRITE FILE
             return CreatedAtAction(nameof(Get), new { id = carDealer.DealerID }, carDealer);
         }
 
@@ -56,42 +125,62 @@ namespace API_Auto_v3._0.Controllers
             }
 
             carDealer.CarList.Add(car);
-
+            //CODICE PER WRITE FILE
+            WriteRecords();
+            //FINE CODICE WRITE FILE
             return Ok();
         }
 
-        // Updating car dealer ID and name
+        // Update car dealer name
         // PUT api/CarDealer/5
         [HttpPut]
-        public ActionResult<CarDealer> Put(int id, CarDealer updatedCarDealer)
+        public IActionResult EditDealer (int id, [FromBody] CarDealer updatedCarDealer)
         {
-            var carDealer = carDealers.FirstOrDefault(d => d.DealerID == id);
-            if (carDealer == null)
-            {
-                return NotFound();
-            }
+            var cardealerToUpdate = carDealers.FirstOrDefault(d => d.DealerID == id);
 
-            carDealer.DealerID = updatedCarDealer.DealerID;
-            carDealer.DealerName = updatedCarDealer.DealerName;
+            if (cardealerToUpdate == null) throw new Exception("Il concessionario non esiste");
 
-            return carDealer;
+            cardealerToUpdate.DealerName = updatedCarDealer.DealerName;
+
+            //CODICE PER WRITE FILE
+            WriteRecords();
+            //FINE CODICE WRITE FILE
+            return Ok();
         }
 
         // Update of a single car object
         // PUT api/CarDealer/5/AA000AA
+
         [HttpPut("{carPlate}")]
         public IActionResult EditCar(string carPlate, [FromBody] Car updatedCar)
         {
+            bool carFound = false; // flag to indicate if the car is found and updated
+
             foreach (var carDealer in carDealers)
             {
                 var carToUpdate = carDealer.CarList.FirstOrDefault(car => car.CarPlate == carPlate);
-                if (carToUpdate == null) throw new Exception("La targa non esiste");
 
-                carToUpdate.CarPlate = updatedCar.CarPlate;
-                carToUpdate.Model = updatedCar.Model;
-                carToUpdate.MaxSpeed = updatedCar.MaxSpeed;
-                carToUpdate.Displacement = updatedCar.Displacement;
+                if (carToUpdate != null)
+                {
+                    carToUpdate.CarPlate = updatedCar.CarPlate;
+                    carToUpdate.Model = updatedCar.Model;
+                    carToUpdate.MaxSpeed = updatedCar.MaxSpeed;
+                    carToUpdate.Displacement = updatedCar.Displacement;
+
+                    carFound = true; // Set the flag to true if the car is found and updated
+                    break; // Exit the loop after updating the car details
+                }
             }
+
+            if (!carFound)
+            {
+                throw new Exception("La targa non esiste"); // Throw an exception if the car is not found
+            }
+
+            //CODICE PER WRITE FILE
+            WriteRecords();
+            //FINE CODICE WRITE FILE
+
             return Ok();
         }
 
@@ -107,6 +196,9 @@ namespace API_Auto_v3._0.Controllers
                 if (carToRemove != null)
                 {
                     carDealer.CarList.Remove(carToRemove);
+                    //CODICE PER WRITE FILE
+                    WriteRecords();
+                    //FINE CODICE WRITE FILE
                     return NoContent();
                 }
             }
